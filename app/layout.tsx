333ffcf7fa417a78af202d45513e043bbf78a07f
Ridwan Maruf <marufridwan997@gmail.com>
2025-11-27 16:56:35 +0100

    updated website description

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ const inter = Inter({
 
 export const metadata: Metadata = {
   title: "Excelia Studio",
-  description: "Excelia Studio Website",
+  description: "Website Design for Business Owners & Founders",
 };
 
 export default function RootLayout({
